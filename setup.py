367af44a86f0d777be15b8b40d4009ebe6769ca8
Blaise Thompson <blaise@untzag.com>
2020-03-19 16:21:30 -0500

    minimum viable set_position

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md") as readme_file:
     readme = readme_file.read()
 
 
-requirements = ["yaqd-core"]
+requirements = ["yaqd-core", "pyserial"]
 
 extra_requirements = {"dev": ["black", "pre-commit"]}
 extra_files = {"yaqd_new_era": ["VERSION"]}
