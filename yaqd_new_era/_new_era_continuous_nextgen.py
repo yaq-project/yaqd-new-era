6c2ad3f6337f0179685f143a48e4c104ab7cc967
kameyer226 <kentmeyer@wisc.edu>
2022-08-10 12:55:23 -0500

    nextgen daemon updated commands, errors

--- yaqd_new_era/_new_era_continuous_nextgen.py
+++ yaqd_new_era/_new_era_continuous_nextgen.py
@@ -7,9 +7,17 @@ from yaqd_core import IsDaemon, HasPosition, IsDiscrete, UsesSerial, UsesUart, a
 
 
 """
-The next generation continuous pumping program works in multiple phases.
+The full command set, alarms, prompts, addressing (for multiple pumps) mechanisms,
+and errors are found in the NE4000 manual.   Additional commands may be found for the X2
+firmware.
 
-When queried (PHN):
+Current daemon assumes only one pump system is in communication with the associated
+serial ("COM") port.   To avoid errors in addressing, all commands are preceded by an
+asterisk which bypasses the requirement for tagging an address to the serial command but
+then does not allow other devices on the serial line.
+
+When queried (*):
+Prompts
 I - infusing
 W - withdrawing
 S - stopped
@@ -17,26 +25,47 @@ P - paused
 T - timed pause
 U - user wait
 X - purging
+
+Alarms
+R - Pump Reset (Power interrupted)
+S - Pump Stall
+T - Serial timeout
+E - Program error
+O - Program phase number out of range
+U - User Commanded Alarm (added)
+
+Errors (Prompt fields)
+? - Command unrecognized
+NA- Command N/A
+OOR Command data out of range
+COM Invalid Serial packet
+IGN Command ignored
 """
 
 
 rate_regex = re.compile(r"([\.\d]+)")
-
 
 class NewEraContinuousNextGen(UsesUart, UsesSerial, IsDiscrete, HasPosition, IsDaemon):
     _kind = "new-era-continuous-nextgen"
 
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
+        #self._state["destination"] = 0.0
+        self._state["current_alarm"]=""
+        self._state["current_prompt"]=""
+        self._state["position_identifier"] = "paused" # possibly to be read from config
         self._ser = aserial.ASerial(
             self._config["serial_port"], baudrate=self._config["baud_rate"], eol="\x03".encode()
         )
         self._rate = None
         self._purging = False
+        self._busy=False
         # the pump ignores the very first RS232 command it sees after power cycling for some reason
         # so we send DIS as a junk command to get things going
         self._ser.write(f"*DIS\r".encode())
+        self.reset_alarm()
         self._loop.create_task(self._get_rate())  # cache the rate later
+        self._cached_alarm= ""
         self._cached_prompt = ""
         self.set_position(self._state.get("destination", 0))
 
@@ -51,7 +80,7 @@ class NewEraContinuousNextGen(UsesUart, UsesSerial, IsDiscrete, HasPosition, IsD
 
     async def _get_rate(self):
         try:
-            prompt, alarm, out = await self._write("RAT")
+            prompt, alarm, out = await self._write("*RAT\r".encode())
             match = rate_regex.match(out)
             self._rate = float(match[1])
         except TypeError:
@@ -66,23 +95,57 @@ class NewEraContinuousNextGen(UsesUart, UsesSerial, IsDiscrete, HasPosition, IsD
 
     def set_rate(self, rate):
         rate = f"{rate:0.3f}"[:5]
-        self._ser.write(f"* RAT {rate}\r".encode())
+        self._ser.write(f"*RAT{rate}\r".encode())
         self._loop.create_task(self._get_rate())
+
+    def set_alarm(self, alarm):
+        """Sets the alarm on the pump."""
+        assert (alarm==True or alarm==False)
+        if alarm:
+            self._ser.write("*OUT51\r".encode())
+            self._ser.write("*BUZ1\r".encode())
+            self._state["current_alarm"]="U"
+            self.logger.info(f"Alarmed by User: time {time.localtime()}")
+        else:
+            self._ser.write("*OUT50\r".encode())
+            self._ser.write("*BUZ0\r".encode())
+            self._state["current_alarm"]=""
+  
+    def get_alarm(self):
+        alarm=self._state["current_alarm"]
+        if (alarm == None or alarm == ""):
+            return False
+        else:
+            return True
 
     async def update_state(self):
         while True:
-            prompt, alarm, out = await self._write("PHN")
-            self._cached_prompt = prompt
+            prompt, alarm, out = await self._write("".encode())
+            if prompt is not None:
+                if self._cached_prompt != prompt:
+                    self._cached_prompt = prompt
+                    self.logger.info(f"prompt change {prompt}: time {time.localtime()}")
+                    self._state["current_prompt"]=prompt
+
+            if alarm is not None:
+                if self._cached_alarm != alarm:
+                    self._cached_alarm = alarm
+                    self.logger.info(f"Alarm {alarm}: time {time.localtime()}")
+                    self._state["current_alarm"]=alarm
+                    prompt, alarm, out = await self._write("BUZ12")
+                    prompt, alarm, out = await self._write("OUT51")
+
             if self._state["destination"] >= 0.5:
-                self._busy = self._cached_prompt not in ["I", "W"]
+                self._busy = prompt not in ["I", "W"]
             else:
-                self._busy = self._cached_prompt in ["I", "W"]
+                self._busy = prompt in ["I", "W"]
             if not self._busy:
                 self._state["position"] = self._state["destination"]
             if self._state["position"] >= 0.5:
                 self._state["position_identifier"] = "pumping"
             else:
                 self._state["position_identifier"] = "paused"
+            await asyncio.sleep(0.2)
 
     async def _write(self, command):
         try:
@@ -93,7 +156,7 @@ class NewEraContinuousNextGen(UsesUart, UsesSerial, IsDiscrete, HasPosition, IsD
             out = f"*{command}\r".encode()
             response = await self._ser.awrite_then_readline(out)
             response = response.decode().strip()
-            self.logger.debug(f"Recieved response: {response}")
+            self.logger.debug(f"Received response: {response}")
             address = int(response[1:3])
             prompt = alarm = data = None
             if response[3] == "A":  # alarm
